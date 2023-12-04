e8ade0ca81b0e6d7d4490a79299121c7cd81adf5
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-12-04 17:54:31 -0300

    Fix migration

--- apps/meteor/server/startup/migrations/v305.ts
+++ apps/meteor/server/startup/migrations/v305.ts
@@ -1,12 +1,16 @@
 import type { IPermission } from '@rocket.chat/core-typings';
 import { Permissions } from '@rocket.chat/models';
 
+import { upsertPermissions } from '../../../app/authorization/server/functions/upsertPermissions';
 import { addMigration } from '../../lib/migrations';
 
 addMigration({
 	version: 305,
 	name: 'Copy roles from add-team-channel permission to new create-team-channel and crteate-team-group permissions',
 	async up() {
+		// Calling upsertPermissions on purpose so that the new permissions are added before the migration
+		await upsertPermissions();
+
 		const addTeamChannelPermission = await Permissions.findOneById<Pick<IPermission, 'roles'>>('add-team-channel', {
 			projection: { roles: 1 },
 		});
