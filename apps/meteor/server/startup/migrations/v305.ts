53a9eb938870b60575fe10588ea900257fd2ebb2
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2023-12-04 18:12:39 -0300

    Update comment

--- apps/meteor/server/startup/migrations/v305.ts
+++ apps/meteor/server/startup/migrations/v305.ts
@@ -8,7 +8,7 @@ addMigration({
 	version: 305,
 	name: 'Copy roles from add-team-channel permission to new create-team-channel and crteate-team-group permissions',
 	async up() {
-		// Calling upsertPermissions on purpose so that the new permissions are added before the migration
+		// Calling upsertPermissions on purpose so that the new permissions are added before the migration runs
 		await upsertPermissions();
 
 		const addTeamChannelPermission = await Permissions.findOneById<Pick<IPermission, 'roles'>>('add-team-channel', {
