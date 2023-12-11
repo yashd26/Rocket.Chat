a185a28a103370203551376daa7e2554d93eeccf
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2023-12-11 12:21:38 -0300

    Fix typecheck

--- apps/meteor/tests/data/rooms.helper.js
+++ apps/meteor/tests/data/rooms.helper.js
@@ -1,6 +1,6 @@
 import { api, credentials, request } from './api-data';
 
-export const createRoom = ({ name, type, username, token, agentId, members, teamId, credentials: customCredentials, voipCallDirection = 'inbound' }) => {
+export const createRoom = ({ name, type, username, token, agentId, members, credentials: customCredentials, teamId = undefined, voipCallDirection = 'inbound' }) => {
 	if (!type) {
 		throw new Error('"type" is required in "createRoom.ts" test helper');
 	}
